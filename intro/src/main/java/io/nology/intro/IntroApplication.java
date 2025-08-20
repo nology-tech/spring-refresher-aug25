3b7dc66dccf35a92e9d7e047910bc181308e11f8
ABaulderstone <alex.baulderstone@gmail.com>
2025-08-20 17:30:44 +0930

    updates

--- intro/src/main/java/io/nology/intro/IntroApplication.java
+++ intro/src/main/java/io/nology/intro/IntroApplication.java
@@ -3,11 +3,17 @@ package io.nology.intro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.nology.intro.greeting.GreetingController;
+import io.nology.intro.greeting.GreetingService;
+import io.nology.intro.greeting.InMemoryNameRepository;
+
 @SpringBootApplication
 public class IntroApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroApplication.class, args);
 	}
+
+	
 
 }
