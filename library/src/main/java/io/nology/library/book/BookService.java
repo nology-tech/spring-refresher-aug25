9a33fbd7b1dc4448f104dbc368a8be520ba1fb4a
ABaulderstone <alex.baulderstone@gmail.com>
2025-08-21 19:33:59 +0930

    updates to use model mapper

--- library/src/main/java/io/nology/library/book/BookService.java
+++ library/src/main/java/io/nology/library/book/BookService.java
@@ -3,19 +3,21 @@ package io.nology.library.book;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import io.nology.library.book.dtos.CreateBookDTO;
 import io.nology.library.book.dtos.UpdateBookDTO;
 import io.nology.library.book.entities.Book;
-import jakarta.validation.Valid;
 
 @Service
 public class BookService {
     private final BookRepository bookRepository;
+    private final ModelMapper mapper;
 
-    public BookService(BookRepository bookRepository) {
+    public BookService(BookRepository bookRepository, ModelMapper mapper) {
         this.bookRepository = bookRepository;
+        this.mapper = mapper;
     }
 
     public List<Book> getAllBooks() {
@@ -27,11 +29,12 @@ public class BookService {
     }
 
     public Book create(CreateBookDTO data) {
-        Book newBook = new Book();
-        newBook.setAuthor(data.getAuthor().trim());
-        newBook.setTitle(data.getTitle().trim());
-        newBook.setGenre(data.getGenre());
-        newBook.setYearPublished(data.getYearPublished());
+        // Book newBook = new Book();
+        // newBook.setAuthor(data.getAuthor().trim());
+        // newBook.setTitle(data.getTitle().trim());
+        // newBook.setGenre(data.getGenre());
+        // newBook.setYearPublished(data.getYearPublished());
+        Book newBook = mapper.map(data, Book.class);
         return this.bookRepository.saveAndFlush(newBook);
     }
 
@@ -55,21 +58,21 @@ public class BookService {
         }
         Book toUpdate = result.get();
 
-        if (data.getAuthor() != null) {
-            toUpdate.setAuthor(data.getAuthor().trim());
-        }
+        // if (data.getAuthor() != null) {
+        // toUpdate.setAuthor(data.getAuthor().trim());
+        // }
 
-        if (data.getTitle() != null) {
-            toUpdate.setTitle(data.getTitle().trim());
-        }
-        if (data.getGenre() != null) {
-            toUpdate.setGenre(data.getGenre());
-        }
+        // if (data.getTitle() != null) {
+        // toUpdate.setTitle(data.getTitle().trim());
+        // }
+        // if (data.getGenre() != null) {
+        // toUpdate.setGenre(data.getGenre());
+        // }
 
-        if (data.getYearPublished() != null) {
-            toUpdate.setYearPublished(data.getYearPublished());
-        }
-
+        // if (data.getYearPublished() != null) {
+        // toUpdate.setYearPublished(data.getYearPublished());
+        // }
+        mapper.map(data, toUpdate);
         this.bookRepository.save(toUpdate);
         return Optional.of(toUpdate);
 
